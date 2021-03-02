54da1a092ff72d984cf37a5e97f421630ba57419
lsj <laisujie@taqu.cn>
2021-03-02 15:50:17 +0800

    ts基础类型认识

--- simple-ts/basic/src/datatype.ts
+++ simple-ts/basic/src/datatype.ts
@@ -17,6 +17,8 @@ function printName(): void{
   // return str;
 }
 
+printName();
+
 /**
 * null和undefined
 * @param name   定义
@@ -26,3 +28,67 @@ let u: undefined = undefined;
 let n: null = null;
 //与void区别，undefined和null是所有类型的子类型。也就是说undefined类型的变量，可以赋值给Number类型变量
 let num: number = u;
+
+/**
+* 定义数组类型
+*/
+{
+  let list: number[] = [1,2,3];
+  let list2: Array<number> = [1,2,3];
+  console.log(JSON.stringify(list) === JSON.stringify(list2));
+
+  //元组数组
+  let x: [string,number];
+  //第一个元素必须是字符串，第二个是数值
+  x = ['1',2];
+  // x[3] = '2'; 
+  console.log(x);
+}
+
+/**
+* 枚举类型
+* @param name   定义
+*/
+{
+  //默认第一项是0，如果修改第一项为1则从1开始
+  enum Color {Red = 1,Green,Blue};
+  let c: Color = Color.Green;
+  //获取green所在的索引
+  console.log(c);
+
+  let colorName : string = Color[2];
+  //获取索引为第二项的枚举名称
+  console.log(colorName);
+}
+
+/**
+* 任意类型
+*/
+{
+  let noSure : any = 4;
+  noSure = 'maybe a string';
+  noSure = false;
+  console.log(noSure);
+}
+
+/**
+* 赋予没有返回值的函数
+*/
+{
+  function warnUser():void{
+    console.log('this is no return fn');
+  }
+
+  warnUser();
+}
+
+/**
+* 断言使用
+*/
+{
+  //两种形式的断言没有区别
+  let someValue : any = 'this is string';
+  let stringLength : number = (<string>someValue).length;
+  let stringLength2 : number = (someValue as string).length;
+  console.log(stringLength,stringLength2);
+}
